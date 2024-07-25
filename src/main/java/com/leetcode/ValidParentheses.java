8f4beccd37018fb233677a8020e996c6444c2919
thi quach <thiquach@gmail.com>
2024-07-25 10:33:48 +1000

    update valid parentheses

--- src/main/java/com/leetcode/ValidParentheses.java
+++ src/main/java/com/leetcode/ValidParentheses.java
@@ -1,53 +1,40 @@
 package main.java.com.leetcode;
 
-import java.util.ArrayList;
+import java.util.Stack;
 
+// Leetcode#20 valid parentheses
 public class ValidParentheses {
-    public static class Stack {
-        static ArrayList<Character> list = new ArrayList<>();
-
-        public boolean isEmpty() {
-            return (list.isEmpty());
-        }
-
-        public void push (char data) {
-            list.add(data);
-        }
-
-        public char pop () {
-            if (list.isEmpty())
-                return '\0';
-
-            char top = list.get(list.size() -1);
-            list.remove(list.size() - 1);
-            return top;
-        }
-    }
-
     public static void main(String[] args) {
-        String ss1 = "(){}[]";
-        System.out.println(ss1 + " isValid? " + isValid(ss1));
+//        String ss1 = "(){}[]";
+//        System.out.println(ss1 + " isValid? " + isValid(ss1));
+//
+//        String ss2 = "([{}])";
+//        System.out.println(ss2 + " isValid? " + isValid(ss2));
+//
+//        String ss3 = "([)]";
+//        System.out.println(ss3 + " isValid " + isValid(ss3));
 
-        String ss2 = "([{}])";
-        System.out.println(ss2 + " isValid? " + isValid(ss2));
-
-        String ss3 = "([)]";
-        System.out.println(ss3 + " isValid " + isValid(ss3));
+        String ss4 = "([}}])";
+        System.out.println(ss4 + " isValid " + isValid(ss4));
     }
 
     public static boolean isValid(String s) {
-        Stack stack = new Stack();
+        if (s.length() % 2 != 0)
+            return false;
+
+        Stack<Character> stack = new Stack<>();
+
         for (char ch : s.toCharArray()) {
             if (ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
+            } else if (ch == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (ch == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (ch == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
             } else {
-                if (stack.isEmpty())
-                    return false;
-                char top = (char) stack.pop();
-                if ((ch == ')' && top != '(') || (ch == ']' && top != '[') ||
-                        (ch == '}' && top != '{')) {
-                    return false;
-                }
+                return false;
             }
         }
         return stack.isEmpty();
