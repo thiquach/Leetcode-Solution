a3a207d132b51082a8d51b8bda6e87bbb0336e0f
thi quach <thiquach@gmail.com>
2024-08-02 06:59:49 +1000

    add smallest infinite set

--- src/main/java/com/leetcode/SmallestInfiniteSet.java
+++ src/main/java/com/leetcode/SmallestInfiniteSet.java
@@ -1,2 +1,37 @@
-package main.java.com.leetcode;public class SmallestInfiniteSet {
+package main.java.com.leetcode;
+
+import java.util.*;
+
+// 2336 Heap Priority Queue
+public class SmallestInfiniteSet {
+    private TreeSet<Integer> numbersSet;
+
+    public SmallestInfiniteSet() {
+        numbersSet = new TreeSet<>();
+        for (int i = 1; i < 1001; i++) {
+            numbersSet.add(i);
+        }
+    }
+
+    public int popSmallest() {
+        int smallest = 0;
+        if (!numbersSet.isEmpty()) {
+            smallest = numbersSet.pollFirst();
+        }
+        return smallest;
+    }
+
+    public void addBack(int num) {
+        if (num > 0 && num <= 1000) {
+            numbersSet.add(num);
+        }
+    }
+
+    public static void main(String[] args) {
+        SmallestInfiniteSet s = new SmallestInfiniteSet();
+        s.addBack(2);
+        System.out.println(s.popSmallest()); // 1
+        System.out.println(s.popSmallest()); // 2
+        System.out.println(s.popSmallest()); // 3
+    }
 }
