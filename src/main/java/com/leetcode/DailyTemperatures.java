25f9a0cde98c7c428b09487b3d7f5b374f6932bb
thi quach <thiquach@gmail.com>
2024-08-11 15:01:34 +1000

    add daily temperature

--- src/main/java/com/leetcode/DailyTemperatures.java
+++ src/main/java/com/leetcode/DailyTemperatures.java
@@ -1,2 +1,31 @@
-package main.java.com.leetcode;public class DailyTemperatures {
+package main.java.com.leetcode;
+
+import javafx.util.Pair;
+
+import java.util.*;
+
+// 739 monotonic stack
+public class DailyTemperatures {
+    public static void main(String[] args) {
+
+        int[] nums = new int[] {73,74,75,71,69,72,76,73};
+        int[] result = dailyTemperatures(nums);
+        System.out.println("Daily Temperatures " + Arrays.toString(result));
+    }
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int len = temperatures.length;
+        int[] result = new int[len];
+        Stack<Pair<Integer, Integer>> stack = new Stack<>();
+
+        for (int i=0; i<len; i++) {
+
+            while (!stack.isEmpty() && stack.peek().getKey() < temperatures[i]) {
+                int index = stack.peek().getValue();
+                stack.pop();
+                result[index] = i - index;
+            }
+            stack.push(new Pair<>(temperatures[i], i));
+        }
+        return result;
+    }
 }
