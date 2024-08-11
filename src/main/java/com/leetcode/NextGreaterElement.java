e91e3539d526e3716a8cccce16ddb2a32546d858
thi quach <thiquach@gmail.com>
2024-08-11 10:41:20 +1000

    add next greater element

--- src/main/java/com/leetcode/NextGreaterElement.java
+++ src/main/java/com/leetcode/NextGreaterElement.java
@@ -2,10 +2,9 @@ package main.java.com.leetcode;
 
 import java.util.*;
 
+// 496 Monotonic stack - easy
 public class NextGreaterElement {
     public static void main(String[] args) {
-//        1,3,5,2,4  &  6,5,4,3,2,1,7
-//        4,1,2 & 1,3,4,2
         int[] num1 = new int[] {4,1,2};
         int[] num2 = new int[] {1,3,4,2};
         int[] result = nextGreaterElement(num1, num2);
@@ -13,30 +12,25 @@ public class NextGreaterElement {
     }
 
     public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
-
         int len = nums1.length;
         int[] result = new int[len];
+        HashMap<Integer, Integer> nums1Map = new HashMap<>();
         for (int i=0; i<len; i++) {
-            result[i] = getNextGreater(nums2, nums1[i]);
+            result[i] = -1;
+            nums1Map.put(nums1[i], i);
         }
-        return result;
-    }
 
-    private static int getNextGreater(int[] nums2, int num) {
-        int result = -1;
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
         for (int i=0; i<nums2.length; i++) {
-            map.put(nums2[i], i);
-        }
-
-        if (map.containsKey(num)) {
-            int index = map.get(num);
-            for (int i=index+1; i<nums2.length; i++) {
-                if (nums2[i] > num) {
-                    result = nums2[i];
-                    break;
+            while (!stack.isEmpty() && stack.peek() < nums2[i]) {
+                int num = stack.peek();
+                if (nums1Map.containsKey(num)) {
+                    int index = nums1Map.get(num).intValue();
+                    result[index] = nums2[i];
                 }
+                stack.pop();
             }
+            stack.push(nums2[i]);
         }
         return result;
     }
