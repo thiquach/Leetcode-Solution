e8f1c6ae0b83671819ceaf360a843c8a7677e2e0
thi quach <thiquach@gmail.com>
2024-11-06 11:56:17 +1100

    add rotate string

--- src/main/java/com/leetcode/RotateString.java
+++ src/main/java/com/leetcode/RotateString.java
@@ -1,2 +1,19 @@
-package main.java.com.leetcode;public class RotateString {
+package main.java.com.leetcode;
+
+public class RotateString {
+    public static void main(String[] args) {
+//        String s = "abcde";
+//        String goal = "cdeab";
+        String s = "abcde";
+        String goal = "abced";
+        System.out.print("rotate string " + s + " " + goal );
+        System.out.println(" -> " + rotateString(s, goal));
+    }
+
+    public static boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length())
+            return false;
+
+        return (s + s).contains(goal);
+    }
 }
