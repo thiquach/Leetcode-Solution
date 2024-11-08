cdeb83aa3fd511682776fb80bd39e6f4401f1497
thi quach <thiquach@gmail.com>
2024-11-08 12:59:26 +1100

    add largest combination

--- src/main/java/com/leetcode/LargestCombination.java
+++ src/main/java/com/leetcode/LargestCombination.java
@@ -1,2 +1,44 @@
-package main.java.com.leetcode;public class LargestCombination {
+package main.java.com.leetcode;
+
+import java.util.Arrays;
+
+// 2275
+public class LargestCombination {
+    public static void main(String[] args) {
+//        int[] candidates = {16,17,71,62,12,24,14};
+        int[] candidates = {8,8};
+        System.out.println("largest combination " + largestCombination(candidates));
+    }
+
+    public static int largestCombination(int[] candidates) {
+        if (candidates.length < 1)
+            return 0;
+
+        int len = candidates.length;
+        int[] bitPositionCount = new int[25];
+        int maxCount = 0;
+        for (int i=0; i<len; i++) {
+            bitPosition(candidates[i], bitPositionCount);
+            System.out.print(" bitPosCount " + candidates[i] + " -> ");
+            System.out.println(" -> " + Arrays.toString(bitPositionCount));
+        }
+        for (int i=0; i<25; i++) {
+            maxCount = Math.max(maxCount, bitPositionCount[i]);
+        }
+        return maxCount;
+    }
+
+    private static void bitPosition(int n, int[] bitPostionCount) {
+        if (n < 1)
+            return;
+
+        int count = 0;
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                bitPostionCount[count]++;
+            }
+            n = n >> 1;
+            count++;
+        }
+    }
 }
