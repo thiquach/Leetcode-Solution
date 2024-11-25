8c669ef7ab2a161437e089322d86ceef800a171c
thi quach <thiquach@gmail.com>
2024-11-25 15:30:28 +1100

    update increasing triplets

--- src/main/java/com/leetcode/IncreasingTriplet.java
+++ src/main/java/com/leetcode/IncreasingTriplet.java
@@ -6,7 +6,11 @@ import java.util.List;
 // 334
 public class IncreasingTriplet {
     public static void main(String[] args) {
-        int[] nums = {1,5, 0, 4, 1, 3};
+//        int[] nums = {1,5, 0, 4, 1, 3};
+//        int[] nums = {1,2,3,4,5};
+//        int[] nums = {5,4,3,2,1};
+//        int[] nums = {1,5,0,4,1,3};
+        int[] nums = {20,100,10,12,5,13};
         System.out.println(" triplet? " + increasingTriplet(nums));
     }
 
@@ -15,17 +19,16 @@ public class IncreasingTriplet {
         if (len < 3)
             return false;
 
-        int j = 0;
-        int k = 0;
-        for (int i=0; i<nums.length - 2; i++) {
-            j = i + 1;
-            k = j + 1;
-            while (k < len) {
-                if ((nums[i] < nums[j]) && (nums[j] < nums[k])) {
-                    return true;
-                } else {
-                    k++;
-                }
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= first) {
+                first = num;
+            } else if (num <= second) {
+                second = num;
+            } else {
+                return true;
             }
         }
         return false;
