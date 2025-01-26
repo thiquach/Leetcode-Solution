5225288ac5611d0d1948df38883d880f0dae170b
thi quach <thiquach@gmail.com>
2025-01-26 16:39:38 +1100

    add does valid array exists

--- src/main/java/com/leetcode/DoesValidArrayExists.java
+++ src/main/java/com/leetcode/DoesValidArrayExists.java
@@ -1,2 +1,17 @@
-package main.java.com.leetcode;public class DoesValidArrayExists {
+package main.java.com.leetcode;
+// 2683
+public class DoesValidArrayExists {
+    public static void main(String[] args) {
+//        int[] derived = {1,1,0};
+        int[] derived = {1,0};
+        System.out.println("does valid array exists " + doesValidArrayExist(derived));
+    }
+
+    public static boolean doesValidArrayExist(int[] derived) {
+        int xorSum = 0;
+        for (int j : derived) {
+            xorSum ^= j;
+        }
+        return xorSum == 0;
+    }
 }
