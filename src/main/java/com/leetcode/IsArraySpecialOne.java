0d11eb01837e1418ff88501e3bf574231b44f207
thi quach <thiquach@gmail.com>
2025-02-03 10:08:04 +1100

    add is array special Leetcode #3151

---
+++ src/main/java/com/leetcode/IsArraySpecialOne.java
@@ -0,0 +1,21 @@
+package main.java.com.leetcode;
+
+public class IsArraySpecialOne {
+    public static void main(String[] args) {
+//        int[] nums = {1};
+//        int[] nums = {2,1,4};
+        int[] nums = {4,3,1,6};
+        System.out.println("is special? " + isArraySpecialOne(nums));
+    }
+
+    public static boolean isArraySpecialOne(int[] nums) {
+        for (int i=0; i<nums.length; i++) {
+            if (i > 0) {
+                if ((nums[i] & 1) == (nums[i-1] & 1)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
