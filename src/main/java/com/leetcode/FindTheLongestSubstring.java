0add7a7b8768b8b47e615f19b7d512e5259778ec
thi quach <thiquach@gmail.com>
2025-02-13 16:49:10 +1100

    add find the longest substring with even vowel count

--- src/main/java/com/leetcode/FindTheLongestSubstring.java
+++ src/main/java/com/leetcode/FindTheLongestSubstring.java
@@ -1,2 +1,39 @@
-package main.java.com.leetcode;public class FindTheLongestSubstring {
+package main.java.com.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindTheLongestSubstring {
+    public static void main(String[] args) {
+//        String s = "eleetminicoworoep";
+//        String s = "leetcodeisgreat";
+        String s = "bcbcbc";
+        System.out.println(s + " longest substring " + findTheLongestSubstring(s));
+    }
+
+    public static int findTheLongestSubstring(String s) {
+        if (s.isEmpty())
+            return 0;
+
+        Map<Integer, Integer> sMap = new HashMap<>();
+        sMap.put(0, -1);
+        int vowelCountMask = 0;
+        int maxLen = 0;
+        for (int i = 0; i<s.length(); i++) {
+            switch (s.charAt(i)) {
+                case 'a': vowelCountMask ^= 1; break;
+                case 'e': vowelCountMask ^= 2; break;
+                case 'i': vowelCountMask ^= 4; break;
+                case 'o': vowelCountMask ^= 8; break;
+                case 'u': vowelCountMask ^= 16; break;
+            }
+
+            if (sMap.containsKey(vowelCountMask)) {
+                maxLen = Math.max(maxLen, i - sMap.get(vowelCountMask));
+            } else {
+                sMap.put(vowelCountMask, i);
+            }
+        }
+        return maxLen;
+    }
 }
