741660db8781d0f370beb5b2c2ad739c1fd378c1
thi quach <thiquach@gmail.com>
2025-02-18 14:04:41 +1100

    update letter combinations

--- src/main/java/com/leetcode/LetterCombinations.java
+++ src/main/java/com/leetcode/LetterCombinations.java
@@ -1,10 +1,9 @@
 package main.java.com.leetcode;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-// 17 Backtracking
+// 17 Backtracking or recursive
 public class LetterCombinations {
     public static void main(String[] args) {
 
@@ -14,39 +13,41 @@ public class LetterCombinations {
     }
 
     public static List<String> letterCombinations(String digits) {
-
-        int len = digits.length();
-        if (len < 1 || len > 4)
-            return new ArrayList<>();
-
-        HashMap<Character, String> digitToLetters = new HashMap<>();
-        digitToLetters.put('2', "abc");
-        digitToLetters.put('3', "def");
-        digitToLetters.put('4', "ghi");
-        digitToLetters.put('5', "jkl");
-        digitToLetters.put('6', "mno");
-        digitToLetters.put('7', "pqrs");
-        digitToLetters.put('8', "tuv");
-        digitToLetters.put('9', "wxyz");
-
         List<String> result = new ArrayList<>();
-        makeCombinations(digits.toCharArray(), 0, digitToLetters, new StringBuilder(), result);
+
+        if (digits.isEmpty())
+            return result;
+
+        String[] mapping = {
+                " ",
+                "",
+                "abc",
+                "def",
+                "ghi",
+                "jkl",
+                "mno",
+                "pqrs",
+                "tuv",
+                "wxyz"
+        };
+
+        StringBuilder sb = new StringBuilder();
+        letterCombinationsRecursion(result, mapping, digits, 0, sb);
         return result;
     }
 
-    public static void makeCombinations(char[] digits, int index, HashMap<Character, String> digitMap, StringBuilder sb, List<String> result) {
-
-        if (index == digits.length) {
+    private static void letterCombinationsRecursion(List<String> result, String[] mapping, String digits, int index, StringBuilder sb) {
+        if (index == digits.length()) {
             result.add(sb.toString());
             return;
         }
 
-        String letterOption = digitMap.get(digits[index]);
-        for (int i=0; i<letterOption.length(); i++) {
-            char letter = letterOption.charAt(i);
-            sb.append(letter);
-            makeCombinations(digits, index + 1, digitMap, sb, result);
-            sb.deleteCharAt(sb.length() - 1); // backtracking
+        int d = digits.charAt(index) - '0';
+        String letters = mapping[d];
+        for (int i=0; i<letters.length(); i++) {
+            sb.append(letters.charAt(i));
+            letterCombinationsRecursion(result, mapping, digits, index + 1, sb);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 }
