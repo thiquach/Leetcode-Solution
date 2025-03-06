6a0e057abd5de095e5582ca3ad73bdeefc63e34c
thi quach <thiquach@gmail.com>
2025-03-06 17:38:06 +1100

    add find missing and repeated values

---
+++ src/main/java/com/leetcode/FindMissingAndRepeatedValues.java
@@ -0,0 +1,42 @@
+package main.java.com.leetcode;
+
+import java.util.*;
+
+public class FindMissingAndRepeatedValues {
+    public static void main(String[] args) {
+//        int[][] grid = {{1, 3}, {2, 2}};
+        int[][] grid = {{9,1,7},{8,9,2},{3,4,6}};
+        System.out.println("Missing repeated " + Arrays.toString(findMissingAndRepeatedValues(grid)));
+    }
+
+    public static int[] findMissingAndRepeatedValues(int[][] grid) {
+        if (grid == null)
+            return new int[]{};
+
+        int numLength = grid.length * grid.length;
+        int total = numLength * (numLength + 1) / 2;
+
+        int[] nums = new int[numLength];
+        int index = 0;
+        int setSum = 0;
+        int pSum = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                nums[index] = grid[i][j];
+                pSum += nums[index];
+                index++;
+            }
+        }
+
+        Set<Integer> numsSet = new HashSet<>();
+        for (int num : nums) {
+            numsSet.add(num);
+        }
+
+        List<Integer> list = new ArrayList<>(numsSet);
+        for (int i = 0; i < list.size(); i++) {
+            setSum += list.get(i);
+        }
+        return new int[]{pSum - setSum, total - setSum};
+    }
+}
