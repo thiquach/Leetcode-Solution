a5a5ffe4919da31060b9c0919c54dab9deab7f16
thi quach <thiquach@gmail.com>
2025-03-12 17:05:57 +1100

    add number of alternating group I

--- src/main/java/com/leetcode/NumberOfAlternatingGroup.java
+++ src/main/java/com/leetcode/NumberOfAlternatingGroup.java
@@ -1,2 +1,28 @@
-package main.java.com.leetcode;public class NumberOfAlternatingGroup {
+package main.java.com.leetcode;
+// 3206
+public class NumberOfAlternatingGroup {
+    public static void main(String[] args) {
+//        int[] colors = {1,1,1};
+//        int[] colors = {0,1,0,0,1};
+        int[] colors = {0,1,0};
+        System.out.println("# of alternating gp " + numberOfAlternatingGroups(colors));
+    }
+
+    public static int numberOfAlternatingGroups(int[] colors) {
+        if (colors.length < 2)
+            return 0;
+
+        int length = colors.length;
+        int result = 0;
+        for (int i=0; i<length; i++ ) {
+            int prev = colors[i];
+            int cur = colors[(i+1+length) % length];
+            int next = colors[(i+2+length) % length];
+            if (prev != cur && cur != next) {
+                result++;
+            }
+        }
+        System.out.println("result " + result);
+        return result;
+    }
 }
